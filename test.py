11ea7a1f856f18a6c41231d378bcbd82948933d9
Allen Wang <allenw@mit.edu>
2019-09-25 17:54:55 -0400

    done at lab 09_25

--- test.py
+++ test.py
@@ -2,13 +2,14 @@ from random_vector import RandomVector, cBetaRandomVariable
 import numpy as np
 from stochastic_verification_functions import StochasticVerificationFunction
 from kinematic_car import KinematicCarAccelNoise
+import time
 
-p = lambda x,y: -2*x**2 - 4*y**2 + 3*y**4 + 2.0*x**4 - 0.1
-n_t = 4
+p = lambda x,y: x**2 + y**2 - 1
+n_t = 5
 dt = 0.05
 alpha = 500
 beta = 550
-x0 = -2.0
+x0 = -3.0
 y0 = 0.0
 v0 = 0
 uaccel_seq = 2*np.ones((1,n_t)).flatten()
@@ -20,4 +21,7 @@ print("MULTINOMIAL RESULTS")
 verification_function = StochasticVerificationFunction(p, KinematicCarAccelNoise, n_t, dt)
 verification_function.compile_moment_functions_multinomial()
 verification_function.set_problem_data(random_vec, theta_seq, x0, y0, v0)
-verification_function.compute_rv_moments()
+start_t = time.time()
+verification_function.compute_prob_bound_multimonial()
+end_t = time.time()
+print("Time to compute : " + str(end_t - start_t))
