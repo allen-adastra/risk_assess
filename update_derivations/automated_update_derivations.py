2c74a9d381a581ee78c1727d21c71f1cbc3378bf
Allen Wang <allenw@mit.edu>
2019-11-04 15:35:44 -0500

    automated_update_derivations.py now use BaseVariable over sympy representaitons

--- update_derivations/automated_update_derivations.py
+++ update_derivations/automated_update_derivations.py
@@ -11,7 +11,7 @@ class BaseVariables(object):
         self._base_variables = base_variables
 
     @property
-    def sympy_rep(self):
+    def sympy_reps(self):
         return [var.sympy_rep for var in self._base_variables]
 
     @property
@@ -40,6 +40,10 @@ class BaseVariable(object):
     @property
     def sympy_rep(self):
         return self._sympy_symbol
+
+    @property
+    def string_rep(self):
+        return str(self._sympy_symbol)
     
     @property
     def max_moment(self):
@@ -47,20 +51,65 @@ class BaseVariable(object):
 
     @property
     def update_relation(self):
+        """
+        NTS: Should be an instance of a sympy polynomial.
+        """
         return self._update_relation
+
+
+"""
+A "DerivedVariable" is a BaseVariable that is created from the product of other base variables.
+"""
+class DerivedVariable(BaseVariable):
+    def __init__(self, component_variables, update_relation):
+        """
+        Args:
+            component_variables: list of instances of BaseVariable s.t. the product of the variables in it form this variable.
+            update_relation: 
+        """
+        variable_string_reps = [str(var.sympy_rep) for var in component_variables]
+        max_moment = 1 # By default, we only know how to compute the moments up to 1 of DerivedVariable
+        super(DerivedVariable, self).__init__(sp.Symbol(' * '.join(variable_string_reps)), max_moment, update_relation)
+        self._component_variables = list(component_variables)
+
+    def __str__(self):
+        return str({var.sympy_rep for var in self.component_variables})
+
+    @property
+    def component_variables(self):
+        return self._component_variables
+
+    @property
+    def component_variables_sympy(self):
+        return [var.sympy_rep for var in self._component_variables]
+
+    def equivalent_variables(self, set_of_base_variables):
+        """
+        set_of_base_variables: is the product of the variables in set_of_base_variables equivalent to this variable?
+        """
+        return set(self.component_variables) == set_of_base_variables
+
+    def sympy_equivalent(self, set_of_sympy_vars):
+        """
+        set_of_sympy_vars: is the product of the sympy vars in this set equivalent to this variable?
+        """
+        return set_of_sympy_vars == {var.sympy_rep for var in self.component_variables}
     
 
-def identify_needed_updates(polynomial, base_variables, dependence_graph, terms_with_generated_relations):
+def identify_needed_updates(derived_variable_to_check, base_variables, dependence_graph, derived_variables):
     """
     Args:
-        polynomial: sympy polynomial in the base variables
+        derived_variable_to_check: instance of DerivedVariable
         base_variables: instance of BaseVariables
-        dependence_graph: 
-        terms_with_generated_relations: 
+        dependence_graph: networkx with nodes as variables in BaseVariables and edges representing the two variables are dependent
+        derived_variables: list of instances of DerivedVariable, should include derived_variable_to_check
+    Returns:
+        new_update_relations_needed: list of sets of sympy variables tha
     """
+    assert derived_variable_to_check in derived_variables
     new_update_relations_needed = []
     variables_need_higher_moments = dict()
-    for mono in polynomial.monoms():
+    for mono in derived_variable_to_check.update_relation.monoms():
         for i, degree in enumerate(mono):
             # Check if we can compute moments of the random variable up to "degree"
             # If not, we will need to derive an update relation for the "degree-th"
@@ -68,7 +117,7 @@ def identify_needed_updates(polynomial, base_variables, dependence_graph, terms_
             if degree > base_variables.max_moments[i]:
                 variables_need_higher_moments[base_variables.variables[i]] = degree
         # Find the graph of variable dependencies for this particular monomial.
-        variables_in_mono = {base_variables.sympy_rep[i] for i, degree in enumerate(mono) if degree != 0}
+        variables_in_mono = {base_variables.variables[i] for i, degree in enumerate(mono) if degree != 0}
         mono_dependence_graph = dependence_graph.subgraph(variables_in_mono)
         if len(mono_dependence_graph.edges) == 0:
             # All variables in this monomial are independent of each other.
@@ -81,53 +130,48 @@ def identify_needed_updates(polynomial, base_variables, dependence_graph, terms_
             connected_components = nx.connected_components(mono_dependence_graph)
             for component in connected_components:
                 component_non_trivial = len(component) > 1 # If there is only one node in the component, it is trivial.
-                update_relation_exists = any([component == group for group in terms_with_generated_relations]) # Does an update relation already exist for this component?
+                update_relation_exists = any([derived_var.equivalent_variables(component) for derived_var in derived_variables]) # Does an update relation already exist for this component?
                 if ((component_non_trivial) and (not update_relation_exists) and (component not in new_update_relations_needed)):
-                    # DOUBLE CHECK.
                     new_update_relations_needed.append(component)
     return new_update_relations_needed, variables_need_higher_moments
 
 def generate_new_update_relations(need_update_relations, base_variables):
     """
     Args:
-        need_update_relations: list of frozensets of variables
+        need_update_relations: list of sets of instances of BaseVariable.
         base_variables: 
     Returns:
-        new_update_relations
+        new_derived_vars
     """
-    new_update_relations = []
-    variable_base_variable_map = {}
-    for var in base_variables.variables:
-        variable_base_variable_map[var.sympy_rep] = var
-    for new_relation_vars in need_update_relations:
+    new_derived_vars = []
+    for list_of_vars in need_update_relations:
+        # Take the product of update relations of variables in list_of_vars.
         new_relation = 1
-        for sympy_var in new_relation_vars:
-            new_relation *= variable_base_variable_map[sympy_var].update_relation
-        new_update_relations.append(sp.poly(new_relation, base_variables.sympy_rep))
-    return new_update_relations
+        for var in list_of_vars:
+            new_relation *= var.update_relation
+        # Make the new relation a sympy polynomial
+        new_relation = sp.poly(new_relation, base_variables.sympy_reps)
+        new_derived_vars.append(DerivedVariable(list_of_vars, new_relation))
+    return new_derived_vars
 
-def iterate_relations(relations_to_check, base_variables, variable_dependence_graph, terms_with_generated_relations):
+def iterate_relations(derived_base_vars_to_check, base_variables, variable_dependence_graph, derived_base_vars):
     """
     Args:
-        relations_to_check: 
+        derived_base_vars_to_check: 
         base_variables: 
         variable_dependence_graph: 
         terms_with_generated_relations: 
     Returns:
-        new_relations_to_check
-        terms_with_generated_relations
+        new_derived_base_vars
+        derived_base_vars
     """
     need_update_relations = []
     # Given a list of relations to check, generate a new set of relations to check.
-    for relation in relations_to_check:
-        new_update_relations_needed, variables_need_higher_moments = identify_needed_updates(relation, base_variables, variable_dependence_graph, terms_with_generated_relations)
+    for relation in derived_base_vars_to_check:
+        new_update_relations_needed, variables_need_higher_moments = identify_needed_updates(relation, base_variables, variable_dependence_graph, derived_base_vars)
         need_update_relations += new_update_relations_needed
 
-    # Eliminate redundant stuff
-    need_update_relations = {frozenset(s) for s in need_update_relations}
-    need_update_relations = [set(s) for s in need_update_relations]
-    
     # Generate new relations based off what we have in need_update_relations
-    new_relations_to_check = generate_new_update_relations(need_update_relations, base_variables)
-    terms_with_generated_relations += need_update_relations
-    return new_relations_to_check, terms_with_generated_relations
+    new_derived_base_vars = generate_new_update_relations(need_update_relations, base_variables)
+    derived_base_vars += new_derived_base_vars
+    return new_derived_base_vars, derived_base_vars
--- update_derivations/automated_update_derivations_test.py
+++ update_derivations/automated_update_derivations_test.py
@@ -1,36 +1,35 @@
-from automated_update_derivations import identify_needed_updates, BaseVariable, BaseVariables
+from automated_update_derivations import identify_needed_updates, BaseVariable, BaseVariables, DerivedVariable, iterate_relations
 import sympy as sp
 import networkx as nx
 
 class TestClass:
     def setup(self):
         # Declare variables at time "t"
-        self._xt = sp.Symbol("x_t")
-        self._yt = sp.Symbol("y_t")
-        self._vt = sp.Symbol("v_t")
-        self._wvt = sp.Symbol("w_{v_t}")
-        self._wthetat = sp.Symbol("w_{theta_t}")
-        self._thetat = sp.Symbol("theta_t")
-        self._cos_thetat = sp.cos(self._thetat)
-        self._sin_thetat = sp.sin(self._thetat)
-        self._sin_thetat = sp.sin(self._thetat)
-        self._cos_thetat = sp.cos(self._thetat)
-        self._sin_wthetat = sp.sin(self._wthetat)
-        self._cos_wthetat = sp.cos(self._wthetat)
+        xt = sp.Symbol("x_t")
+        yt = sp.Symbol("y_t")
+        vt = sp.Symbol("v_t")
+        wvt = sp.Symbol("w_{v_t}")
+        wthetat = sp.Symbol("w_{theta_t}")
+        thetat = sp.Symbol("theta_t")
+        cos_thetat = sp.cos(thetat)
+        sin_thetat = sp.sin(thetat)
 
-        self._xt_base = BaseVariable(self._xt, 1,  self._xt + self._vt * self._cos_thetat)
-        self._yt_base = BaseVariable(self._yt, 1, self._yt + self._vt * self._sin_thetat)
-        self._vt_base = BaseVariable(self._vt, 2, self._vt + self._wvt)
-        self._sin_thetat_base = BaseVariable(self._sin_thetat, 1000, sp.expand_trig(sp.sin(self._thetat + self._wthetat)))
-        self._cos_thetat_base = BaseVariable(self._cos_thetat, 1000, sp.expand_trig(sp.cos(self._thetat + self._wthetat)))
-        self._sin_wthetat_base = BaseVariable(self._sin_wthetat, 0, None)
-        self._cos_wthetat_base = BaseVariable(self._cos_wthetat, 0, None)
+        sin_wthetat = sp.sin(wthetat)
+        cos_wthetat = sp.cos(wthetat)
 
-        self._base_variables = BaseVariables([self._xt_base, self._yt_base, self._vt_base, self._sin_thetat_base,
-                                            self._cos_thetat_base, self._sin_wthetat_base, self._cos_wthetat_base])
+        self._xt = BaseVariable(xt, 1,  xt+ vt * cos_thetat)
+        self._yt = BaseVariable(yt, 1, yt + vt * sin_thetat)
+        self._vt = BaseVariable(vt, 2, vt + wvt)
+        self._sin_thetat = BaseVariable(sin_thetat, 1000, sp.expand_trig(sp.sin(thetat + wthetat)))
+        self._cos_thetat = BaseVariable(cos_thetat, 1000, sp.expand_trig(sp.cos(thetat + wthetat)))
+        self._sin_wthetat = BaseVariable(sin_wthetat, 0, None)
+        self._cos_wthetat = BaseVariable(cos_wthetat, 0, None)
+
+        self._base_variables = BaseVariables([self._xt, self._yt, self._vt, self._sin_thetat,
+                                            self._cos_thetat, self._sin_wthetat, self._cos_wthetat])
 
         self._variable_dependence_graph = nx.Graph()
-        self._variable_dependence_graph.add_nodes_from(self._base_variables.sympy_rep)
+        self._variable_dependence_graph.add_nodes_from(self._base_variables.variables)
         self._variable_dependence_graph.add_edges_from([(self._xt, self._yt), (self._xt, self._vt), (self._yt, self._vt),
                                                         (self._xt, self._sin_thetat), (self._xt, self._cos_thetat), (self._yt, self._sin_thetat),
                                                         (self._yt, self._cos_thetat)])
@@ -38,15 +37,60 @@ class TestClass:
 
     def test_cross(self):
         self.setup()
-        terms_with_generated_relations = [set([self._xt, self._sin_thetat]), set([self._xt, self._cos_thetat]),
-                                          set([self._yt, self._sin_thetat]), set([self._yt, self._cos_thetat])]
-
+        derived_base_variables = [DerivedVariable([self._xt, self._sin_thetat], None),
+                                  DerivedVariable([self._xt, self._cos_thetat], None),
+                                  DerivedVariable([self._yt, self._sin_thetat], None),
+                                  DerivedVariable([self._yt, self._cos_thetat], None)]
         # Construct the expression for E[x_{t+1} * y_{t+1}] in terms of expressions at time t
-        cross = sp.poly(self._xt_base.update_relation * self._yt_base.update_relation, self._base_variables.sympy_rep)
-        terms_with_generated_relations += [set([self._xt, self._yt])]
-        new_update_relations_needed, variables_need_higher_moments = identify_needed_updates(cross, self._base_variables, self._variable_dependence_graph, terms_with_generated_relations)
-
+        cross_update = sp.poly(self._xt.update_relation * self._yt.update_relation, self._base_variables.sympy_reps)
+        cross = DerivedVariable([self._xt, self._yt], cross_update)
+        derived_base_variables += [cross]
+        new_update_relations_needed, variables_need_higher_moments = identify_needed_updates(cross, self._base_variables, self._variable_dependence_graph, derived_base_variables)
         assert len(new_update_relations_needed) == 2
         assert ({self._xt, self._vt, self._sin_thetat} in new_update_relations_needed)
         assert ({self._yt, self._vt, self._cos_thetat} in new_update_relations_needed)
         assert len(variables_need_higher_moments) == 0
+
+    def test_iterate_relations(self):
+        self.setup()
+
+        # From the Fall 2019 ICRA Paper, we have the following update relations:
+        derived_base_variables = [DerivedVariable([self._xt, self._sin_thetat], None),
+                                  DerivedVariable([self._xt, self._cos_thetat], None),
+                                  DerivedVariable([self._yt, self._sin_thetat], None),
+                                  DerivedVariable([self._yt, self._cos_thetat], None)]
+        # Construct the expression for E[x_{t+1} * y_{t+1}] in terms of expressions at time t
+        cross_update = sp.poly(self._xt.update_relation * self._yt.update_relation, self._base_variables.sympy_reps)
+        cross = DerivedVariable([self._xt, self._yt], cross_update)
+        derived_base_variables += [cross]
+        derived_base_variables_to_check = [cross]
+
+        iters = 0
+        while derived_base_variables_to_check:
+            derived_base_variables_to_check, derived_base_variables = iterate_relations(derived_base_variables_to_check, self._base_variables, self._variable_dependence_graph, derived_base_variables)
+            iters += 1
+
+        # It should take three iterations for the given problem.
+        assert iters == 3 
+        # The resulting derived variables should be the following nine, and no others.
+        derived_base_variables_components = [set(var.component_variables) for var in derived_base_variables]
+        assert {self._xt, self._sin_thetat} in derived_base_variables_components
+        assert {self._xt, self._cos_thetat} in derived_base_variables_components
+        assert {self._yt, self._sin_thetat} in derived_base_variables_components
+        assert {self._yt, self._cos_thetat} in derived_base_variables_components
+        assert {self._xt, self._yt} in derived_base_variables_components
+        assert {self._xt, self._vt, self._sin_thetat} in derived_base_variables_components
+        assert {self._xt, self._vt, self._cos_thetat} in derived_base_variables_components
+        assert {self._yt, self._vt, self._sin_thetat} in derived_base_variables_components
+        assert {self._yt, self._vt, self._cos_thetat} in derived_base_variables_components
+        assert len(derived_base_variables_components) == 9
+
+    def test_derived_base_var(self):
+        x = BaseVariable(sp.Symbol('x'), 1, None)
+        y = BaseVariable(sp.Symbol('y'), 1, None)
+        x_times_y = DerivedVariable([x, y], None)
+        assert x_times_y.string_rep == 'x * y' # Syntax for the string representation of instances of DerivedVariable
+        assert x_times_y.component_variables == [x, y]
+        assert x_times_y.sympy_equivalent({x.sympy_rep, y.sympy_rep}) == True
+        assert x_times_y.sympy_equivalent({y.sympy_rep, x.sympy_rep}) == True
+        assert x_times_y.sympy_equivalent({x.sympy_rep, y.sympy_rep, sp.Symbol('z')}) == False
