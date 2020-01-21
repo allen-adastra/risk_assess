44e9e1ed1ca709cdebb841bfe08289b0d5e69709
allen <allenw@mit.edu>
2020-01-20 23:16:10 -0500

    maybe the new test1 works?

---
+++ update_derivations/test_new_update_derivations.py
@@ -0,0 +1,58 @@
+from new_update_derivations import *
+import sympy as sp
+import networkx as nx
+
+class TestThing(object):
+    def setup(self):
+        # Declare variables at time "t"
+        xt = sp.Symbol("x_t")
+        yt = sp.Symbol("y_t")
+        vt = sp.Symbol("v_t")
+        wvt = sp.Symbol("w_{v_t}")
+        wthetat = sp.Symbol("w_{theta_t}")
+        thetat = sp.Symbol("theta_t")
+        cos_thetat = sp.cos(thetat)
+        sin_thetat = sp.sin(thetat)
+        sin_wthetat = sp.sin(wthetat)
+        cos_wthetat = sp.cos(wthetat)
+
+        # Initialize base variables.
+        self._xt = BaseVariable(xt, 1,  xt + vt * cos_thetat)
+        self._yt = BaseVariable(yt, 1, yt + vt * sin_thetat)
+        self._vt = BaseVariable(vt, 2, vt + wvt)
+        self._sin_thetat = BaseVariable(sin_thetat, float("inf"), sp.expand_trig(sp.sin(thetat + wthetat)))
+        self._cos_thetat = BaseVariable(cos_thetat, float("inf"), sp.expand_trig(sp.cos(thetat + wthetat)))
+        self._sin_wthetat = BaseVariable(sin_wthetat, float("inf"), None)
+        self._cos_wthetat = BaseVariable(cos_wthetat, float("inf"), None)
+
+        self._base_variables = [self._xt, self._yt, self._vt, self._sin_thetat, self._cos_thetat, self._sin_wthetat, self._cos_wthetat]
+
+        self._variable_dependence_graph = nx.Graph()
+        self._variable_dependence_graph.add_nodes_from(self._base_variables)
+        self._variable_dependence_graph.add_edges_from([(self._xt, self._yt), (self._xt, self._vt), (self._yt, self._vt),
+                                                        (self._xt, self._sin_thetat), (self._xt, self._cos_thetat), (self._yt, self._sin_thetat),
+                                                        (self._yt, self._cos_thetat)])
+
+        # These were derived in the ICRA paper.
+        derived_variables = {DerivedVariable({self._xt : 1, self._sin_thetat : 1}, None),
+                                    DerivedVariable({self._xt : 1, self._cos_thetat : 1}, None),
+                                    DerivedVariable({self._yt : 1, self._sin_thetat : 1}, None),
+                                    DerivedVariable({self._yt : 1, self._cos_thetat : 1}, None)}
+        
+
+
+    def test1(self):
+        test_update = sp.poly(self._xt.update_relation**4, [var.sympy_rep for var in self._base_variables])
+        res = test_iterate(test_update, self._base_variables, self._variable_dependence_graph, derived_variables)
+    
+    def test_eq_hash(self):
+        foo1 = DerivedVariable({self._xt : 1, self._sin_thetat : 1}, None)
+        foo2 = DerivedVariable({self._sin_thetat : 1, self._xt : 1}, None)
+        assert foo1 == foo2
+        set1 = {foo1}
+        assert (foo2 in set1)
+        set1.add(foo2)
+        assert len(set1) == 1
+tt = TestThing()
+tt.setup()
+tt.test_eq_hash()
