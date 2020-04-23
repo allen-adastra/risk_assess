clean up0, fixed bug with risk assessor concentration inequality risk calculation

risk_assess/random_objects/gmm_trajectory.py
@@ -111,7 +111,7 @@ class GmmTrajectory(object):
             ego_vehicle_position = np.array([[xs[i]],
                                              [ys[i]]])
             rot_mat = rotation_matrix(-thetas[i])
-            gmm = deepcopy(gmms[i])
+            gmm = GMM([deepcopy(comp) for comp in zip(gmms[i].component_probabilities, gmms[i].component_random_variables)])
             gmm.change_frame(ego_vehicle_position, rot_mat)
             gmm_quad_forms[i] = GmmQuadForm(Q, gmm)
         return gmm_quad_forms

risk_assess/random_objects/quad_forms.py
@@ -216,6 +216,10 @@ class GmmQuadForm(object):
         """
         self._mvn_components = [(prob, MvnQuadForm(np.copy(A), mvn)) for prob, mvn in zip(gmm.component_probabilities, gmm.component_random_variables)]
     
+    @property
+    def mvn_components(self):
+        return self._mvn_components
+
     def component_upper_tail_probs(self, t, method, overshoot_one_tolerance = 1e-6, **kwargs):
         """
         Compute the component probabilities:
@@ -231,11 +235,9 @@ class GmmQuadForm(object):
             P(Q > t)
         """
         upper_tail_prob = 0
-        i = 0
         for component_weight, mvnqf in self._mvn_components:                
             mvnqf_prob = mvnqf.upper_tail_probability(t, method, **kwargs)
             upper_tail_prob += component_weight * mvnqf_prob
-            i += 1
         # The calculated probability will have an associated numerical error. Check
         # that the numerical error does not exceed the tolerable amount.
         assert upper_tail_prob < 1.0 + overshoot_one_tolerance

risk_assess/risk_assessor.py
@@ -48,20 +48,18 @@ def evaluate_component(initial_agent_state, half_space_sets, input):
     return component_probs
 
 class RiskAssessor(object):
-    def __init__(self, xs, ys, vs, thetas, car_coord_ellipse):
+    def __init__(self, xs, ys, vs, thetas):
         """
         Args:
             xs : sequence of x positions
             ys : seqeunce of y positions
             vs : sequence of speeds
             thetas : sequence of headings in the global frame
-            car_coord_ellipse: instance of Ellipse defining an ellipse in the cars coordinates. x_center, y_center, and theta should be zero.
         """
         self.xs = xs
         self.ys = ys
         self.vs = vs
         self.thetas = thetas
-        self.car_coord_ellipse = car_coord_ellipse
 
     def generate_ellipses(self, car_coord_ellipse):
         """
@@ -77,94 +75,11 @@ class RiskAssessor(object):
             ellipses[i] = Ellipse(cce.a, cce.b, x + cce.x_center, y + cce.y_center, t + cce.theta)
         return ellipses
 
-    def control_assess_risk_monte_carlo(self, gmm_control_sequence, x0, y0, v0, theta0, n_samples, dt):
-        """
-        Args:
-            gmm_control_sequence (instance of GmmControlSequence)
-            x0 (scalar): initial x position of the agent
-            y0 (scalar): initial y position of the agent
-        """
-        t_start = time.time()
-
-        # Sample control sequences and propagate them into states.
-        accel_samps, steer_samps = gmm_control_sequence.sample(n_samples)
-        agent_xs, agent_ys, _, _ = simulate_deterministic(x0, y0, v0, theta0, accel_samps, steer_samps, dt)
-
-        # Propagating controls introduces "extra steps". Limit ourselves to this number of steps.
-        n_steps = len(self.xs) - 2
-
-        # Ellipse parameters in car coordinates.
-        Q = np.zeros((2, 2))
-        Q[0][0] = 1.0/(self.car_coord_ellipse.a**2)
-        Q[1][1] = 1.0/(self.car_coord_ellipse.b**2)
-
-        # Risks at each time step.
-        risks = n_steps * [None]
-
-        # We will have to transform the position samples into the car coordinates at each time step.
-        for i in range(n_steps):            
-            ego_vehicle_position = np.array([[self.xs[i]],
-                                             [self.ys[i]]])
-            rot_mat = rotation_matrix(-self.thetas[i])
-
-            # Each column is a sample [x; y]
-            agent_xys = np.vstack((agent_xs[:, i], agent_ys[:, i]))
-            assert agent_xys.shape[0] == 2 # Each column is a sampled position
-            change_frame_func = lambda vec : change_frame(vec, ego_vehicle_position, rot_mat)
-            agent_xys_frame_changed = np.apply_along_axis(change_frame_func, 0, agent_xys) # Change the frame of each column
-            # Evaluate the number of samples for which x'Qx <= 1 (i.e: collides with ellipse)
-            res = (agent_xys_frame_changed.T.dot(Q)*agent_xys_frame_changed.T).sum(axis=1)
-            n_collision = np.argwhere(res <= 1.0).size
-            risks[i] = float(n_collision)/float(n_samples)
-        t_total = time.time() - t_start
-        return risks, agent_xs, agent_ys, t_total
-
-    def control_assess_risk_chebyshev_halfspace(self, gmm_control_sequence, initial_agent_state, n_lines, dt):
-        accel_seqs = deepcopy(gmm_control_sequence.array_rep["accels"])
-        steer_seqs = deepcopy(gmm_control_sequence.array_rep["steers"])
-        weights = gmm_control_sequence.array_rep["weights"]
-
-        # Scale down accels and steers by dt.
-        for accel_seq in accel_seqs:
-            for normal in accel_seq:
-                normal.scale(dt)
-        for steer_seq in steer_seqs:
-            for normal in steer_seq:
-                normal.scale(dt)
-
-        # Scale speed in initial_agent_state.
-        initial_agent_state = initial_agent_state.speed_scaled(dt)
-
-        # Generate half space sets.
-        ts = np.linspace(0, 2 * math.pi, n_lines)
-        ellipses = self.generate_ellipses(self.car_coord_ellipse)
-        half_space_sets = len(ellipses) * [None]
-        for i, e in enumerate(ellipses):
-            half_space_sets[i] = set(e.generate_halfspaces_containing_ellipse(ts))
-
-
-        # Time the time it takes to propagate the moments and apply Chebyshev's inequality.
-        pool = multiprocessing.Pool()
-        t_start = time.time()
-        evaluate_component_func = partial(evaluate_component, initial_agent_state, half_space_sets)
-        component_probs = pool.map(evaluate_component_func, list(zip(accel_seqs, steer_seqs, weights)))
-        prob_bounds = len(component_probs[0]) * [0]
-        for weight, comp in zip(weights, component_probs):
-            for i, prob in enumerate(comp):
-                if comp[i] == None:
-                    prob_bounds[i] = None
-                if prob_bounds[i] != None:
-                    prob_bounds[i] += weight * comp[i]
-        t_total = time.time() - t_start
-        pool.close()
-        pool.join()
-        return prob_bounds, t_total
-
     def assess_risk_gmms(self, gmm_quad_forms, method, **kwargs):
         """
         Given a list of gaussian mixture models (GMMs) assess the risk of this plan
         Args:
-            gmm_traj (instance of GmmTrajectory): gmm trajectory in the global frame
+            gmm_quad_forms (list of instances of GmmQuadForm):
             method (string): method used to assess risk
         Returns:
             list of risks associated to the GMMs.
@@ -186,12 +101,19 @@ class RiskAssessor(object):
             inequality_func = gauss
         else: 
             raise Exception("Invalid concentration inequality type.")
-        first_moments = [gmm_qf.compute_moment(1, 1) for gmm_qf in gmm_quad_forms]
-        second_moments = [gmm_qf.compute_moment(1, 2) for gmm_qf in gmm_quad_forms]
-        variances = [second - first**2.0 for first, second in zip(first_moments, second_moments)]
-        risk_bounds = [inequality_func(mean, var) for mean, var in zip(first_moments, variances)]
-        return risk_bounds
 
+        risk_bounds = len(gmm_quad_forms) * [None]
+        for i, gmm_qf in enumerate(gmm_quad_forms):
+            # For each GMMQF, compute the overall risk bound
+            # by taking the weighted sum of risk bounds on the components.
+            gmm_qf_rb = 0.0
+            for weight, mvnqf in gmm_qf.mvn_components:
+                first_moment = mvnqf.compute_moment(1, 1)
+                second_moment = mvnqf.compute_moment(1, 2)
+                variance = second_moment - first_moment**2.0
+                gmm_qf_rb += weight * inequality_func(first_moment, variance)
+            risk_bounds[i] = gmm_qf_rb
+        return risk_bounds
 
     def gmm_quad_form_moments_to_matfile(self, gmm_quad_forms, directory, scenario_number):
         n_components = len(gmm_quad_forms[0]._mvn_components)