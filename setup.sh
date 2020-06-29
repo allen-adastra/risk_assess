931535bdb804b89b0d7d9620fbafffd4c82e3550
allen <allenw@mit.edu>
2020-06-29 10:13:41 -0500

    upd setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+# Install the GNU Scientific Library
+sudo apt-get update
+sudo apt-get install gsl-bin libgsl0-dev
+sudo apt-get install g++
+
+# Activate the virtual environment and install this package.
 source venv/bin/activate
 pip install -e .
+
+# Make a temporary directory for output.
 mkdir /tmp/argoverse
