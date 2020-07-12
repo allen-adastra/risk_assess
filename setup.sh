5464fe5cbd5e8bda4d4c38ea32cfef96a7f6f5be
allen <allenw@mit.edu>
2020-07-12 16:20:54 -0400

    new method for setting up venv

--- setup.sh
+++ setup.sh
@@ -1,11 +1,26 @@
+# Go to the repo root 
+cd $(git rev-parse --show-toplevel)
+
 # Install the GNU Scientific Library
 sudo apt-get update
 sudo apt-get install gsl-bin libgsl0-dev
 sudo apt-get install g++
 
-# Activate the virtual environment and install this package.
+# Create the virtual environment
+python3 -m pip install --user virtualenv
+python3 -m venv venv
 source venv/bin/activate
-pip install -e .
+python -m pip install -e .
+python -m pip install -r requirements.txt
+
+# Install the argoverse-api, which is included as
+# a submodule
+git submodule update --init --recursive
+cd argoverse-api
+git checkout master
+python -m pip install -e .
+cd ..
+
 
 # Make a temporary directory for output.
 mkdir /tmp/argoverse
